819502d8a03c4e2c28fdf8c14df6a9f4da2b9756
Thai Truong <sontruong@hotmail.co.uk>
2021-03-10 00:11:26 +0000

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header className="bg-red-600">
+      <nav>
+        <ul>
+          <li>About me</li>
+          <li>Projects</li>
+          <li>Blogs</li>
+          <li>Contact me</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
