6d0ff216617b71923bee276b6f9c9ecf9638213f
Thai Truong <sontruong@hotmail.co.uk>
2021-03-24 22:38:36 +0000

    about me changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,31 @@
 function About() {
   return (
     <div>
-      <section className="bg-primary p-6  text-center py-24 px-20 md:px-32 lg:px-72 xl:px-80">
+      <section className="bg-primary p-6  text-center py-24 px-12 md:px-32 lg:px-72 xl:px-80">
         <h2 id="aboutMe" className="py-2 text-3xl font-bold text-white">
           Welcome to my website
         </h2>
         <p className=" py-2 text-white ">
-          I currently work in the public sector, where I’m responsible for
-          managing and maintaining the website. I have a good knowledge of WCAG
-          2.1, experience in using CMS Drupal, HTML, CSS, Javascript, Jira,
-          working in Agile, Github, SASS.
+          Enthusiastic self-front-end developer with a focus on building robust
+          and accessible interfaces. I am knowledgeable in HTML, CSS,
+          JavaScript, React and Drupal, with a passion to build web applications
+          and learning new technologies.
         </p>
       </section>
       <section className=" max-w-3xl relative -inset-y-12 m-auto mb-6 container ">
         <div className="flex justify-around shadow-lg pb-4 bg-white  rounded-lg">
           <div className=" p-2 flex flex-col pb-4  ">
             <h3 className="text-xl font-bold text-secondary py-8 pb-4">
-              Front-end developer
+              Front-end
             </h3>
             <ul className="text-tertiary">
               <li>HTML</li>
               <li>CSS</li>
               <li>Javascript</li>
-              <li>SASS</li>
-              <li>Tailwind</li>
+              <li>SCSS/SASS</li>
+              <li>React</li>
+              <li>NPM</li>
+              <li>Tailwind CSS</li>
             </ul>
           </div>
 
@@ -32,11 +34,13 @@ function About() {
               Skills
             </h3>
             <ul class="text-tertiary">
+              <li>Github</li>
               <li>WCAG 2.1</li>
+              <li>Visual studio code</li>
               <li>Jira</li>
               <li>Figma</li>
-              <li>Storybook</li>
               <li>CMS - Drupal</li>
+              <li>Agile / Waterfall</li>
             </ul>
           </div>
         </div>
